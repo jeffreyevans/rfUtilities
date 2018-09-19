f77bd6ed9157dba973781d97260982f70dfd6c2c
Jeffrey S. Evans <jeffreyevans@users.noreply.github.com>
2018-09-19 10:49:29 -0600

    Update accuracy.R

--- R/accuracy.R
+++ R/accuracy.R
@@ -7,33 +7,39 @@
 #' @return A list class object with the following components:
 #' \itemize{ 
 #' \item   PCC                    percent correctly classified (accuracy)
+#' \item   auc                    Area Under the ROC Curve
 #' \item   users.accuracy         The users accuracy  
 #' \item   producers.accuracy     The producers accuracy
 #' \item   kappa                  Cohen's Kappa (chance corrected accuracy)
+#' \item   true.skill             Hanssen-Kuiper skill score (aka true score statistic)
 #' \item   sensitivity            Sensitivity (aka, recall)
 #' \item   specificity            Specificity 
 #' \item   plr                    Positive Likelihood Ratio   
 #' \item   nlr                    Negative Likelihood Ratio  
-#' \item   typeI.error            Type I error
-#' \item   typeII.error           Type II error
-#' \item   gain                   Information gain (aka precision)
+#' \item   typeI.error            Type I error (omission)
+#' \item   typeII.error           Type II error (commission)
+#' \item   gini                   Gini entropy index
 #' \item   f.score                F-score
-#' \item   auc                    Area Under the ROC Curve
+#' \item   gain                   Information gain (aka precision)
+#' \item   mcc                    Matthew's correlation 
 #' \item   confusion              A confusion matrix 
 #'  }
 #' 
-#' @note sensitivity = true positives / ( true positives + false positives ) 
-#' @note specificity = true negatives / ( true negatives + false positives ) 
-#' @note Type I error = 1 - specificity
-#' @note Type II error = 1 - sensitivity
-#' @note Positive Likelihood Ratio  = sensitivity / (1 - specificity) 
-#' @note Negative Likelihood Ratio  = (1 - sensitivity) / specificity
-#' @note gain  = sensitivity / ( (true positives + true negatives) / n )
-#' @note auc = (tpr - fpr + 1) / 2
-#' @note F-Score = 2 * (precision * recall) / (precision + recall) 
-#' @note Hanssen-Kuiper skill score (aka true score statistic) = [(tp * tn) - (fp * fn)] / [(tp + fn) + (fp + tn)]
-#' @note The true skill score has an expected -1 to +1, with 0 representing no discrimination.   
-#' 
+#' @note
+#'   \itemize{ 
+#'   \item   sensitivity = true positives / ( true positives + false positives ) 
+#'   \item   specificity = true negatives / ( true negatives + false positives ) 
+#'   \item   Type I error = 1 - specificity
+#'   \item   Type II error = 1 - sensitivity
+#'   \item   Positive Likelihood Ratio  = sensitivity / (1 - specificity) 
+#'   \item   Negative Likelihood Ratio  = (1 - sensitivity) / specificity
+#'   \item   gain  = sensitivity / ( (true positives + true negatives) / n )
+#'   \item   auc = (tpr - fpr + 1) / 2
+#'   \item   F-Score = 2 * (precision * recall) / (precision + recall) 
+#'   \item   Hanssen-Kuiper skill score (aka true score statistic) = [(tp * tn) - (fp * fn)] / [(tp + fn) + (fp + tn)], The true skill score has an expected -1 to +1, with 0 representing no discrimination.   
+#'  } 
+#' @note Using the table function matrix positions for a 2x2 confusion matrix are TP(1), FN(3), FP(2), TN(4)
+#'
 #' @author Jeffrey S. Evans    <jeffrey_evans<at>tnc.org>
 #'
 #' @references
@@ -59,9 +65,11 @@ accuracy <- function (x, y) {
   } else {
     t.xy <- table(x, y)
   }
-    if(inherits(t.xy, "matrix")) { 
-      rownames(t.xy) <- c("0","1")
-	  colnames(t.xy) <- c("0","1")
+    if(inherits(t.xy, "matrix")) {
+      if(is.null(rownames(t.xy))) {	
+        rownames(t.xy) <- c("0","1")
+	    colnames(t.xy) <- c("0","1")
+	  }
     }
 	tc <- match(colnames(t.xy), rownames(t.xy))
 	mtc <- matrix(ncol = ncol(t.xy), nrow = length(tc[tc == "NA"]), 0)
@@ -81,56 +89,60 @@ accuracy <- function (x, y) {
     t4 <- t(t3[mc, ])
       agree <- diag(t4)
         prod1 <- apply(t4, 1, sum)
-          prod2 <- agree/prod1
+          prod2 <- agree / prod1
              user1 <- apply(t4, 2, sum)
-            user2 <- agree/user1
+            user2 <- agree / user1
           N <- sum(t4)
         k1 <- sum(agree)
       k2 <- sum(prod1 * user1)
-    khat <- abs(((N * k1) - k2)/(N^2 - k2))
-	
-    if( dim(t.xy)[1] == 2 ) {	
-      n = sum(t.xy)
-      prevalence <- t.xy[1] / n 
-      precision <- t.xy[1] / (t.xy[1] + t.xy[3])  
-      tpr <- t.xy[1] / ( t.xy[1] +  t.xy[2] )    
-      tnr <- t.xy[4] / ( t.xy[4] +  t.xy[3] )    
-      type1.error <- 1 - tnr                     
+    khat <- abs(((N * k1) - k2) / (N^2 - k2))	
+    if( dim(t.xy)[1] == 2 ) { 	
+      n = sum(t.xy)  # N	
+      TP <- t.xy[1]  # True Positives, Power 
+	  FN <- t.xy[3]  # False Negatives, Type-II error 
+	  FP <- t.xy[2]  # False Positives, Type-I error 
+	  TN <- t.xy[4]  # True Negatives
+      # prevalence <- TP / n
+      precision <- TP / (TP + FP)  	  
+	  tpr <- TP / (TP + FN)  # true positive rate (aka, sensitivity, recall)
+	  tnr <- TN / (TN + FP)  # true negative rate (aka, specificity, selectivity)	 
+	  fpr <- FP / (FP + TN) 
+	  fnr <- FN / (FN + TP)  # Beta       
+	  type1.error <- 1 - tnr                     
       type2.error <- 1 - tpr                     
       plr <- tpr / (1 - tnr)                     
-      nlr <- (1 - tpr) / tnr                     
-      f.score <- 2 * (precision * tpr) / (precision + tpr)
+      nlr <- (1 - tpr) / tnr 
+	  auc <- (tpr - fpr + 1) / 2
+	  gini <- 2 * auc - 1  
+	  f.score <- 2 * (precision * tpr) / (precision + tpr)
 	  true.skill <- ( (t.xy[1] * t.xy[4]) - (t.xy[3] * t.xy[2]) ) / 
-	                  ( (t.xy[1] + t.xy[2]) * (t.xy[3] + t.xy[4]) )
-	  gain <- precision / ( (t.xy[1] + t.xy[4]) / n ) 
-	  mcc <- ( t.xy[1] * t.xy[4] - t.xy[3] * t.xy[2] ) /
-              sqrt( (t.xy[1] + t.xy[3]) * 
-			    (t.xy[1] + t.xy[2]) * 
-			    (t.xy[4] + t.xy[3]) * 
-			    (t.xy[4] + t.xy[2]) )
-      auc <- (tpr - ( t.xy[3] / ( t.xy[2] +  t.xy[3] ) ) + 1) / 2
+	                ( (t.xy[1] + t.xy[2]) * (t.xy[3] + t.xy[4]) )
+	  gain <- precision / ( (t.xy[1] + t.xy[4]) / n )
+	  mcc <- (TP * TN - FP * FN) / sqrt( (TP + FP) * (TP + FN) * 
+			 (TN + FP) * (TN + FN) )	  
 	  acc <- list( PCC = (sum(diag(t4))/sum(t4)) * 100,
-                 auc = auc, 	
-	             users.accuracy = round(user2 * 100, 1),  
-	             producers.accuracy = round(prod2 * 100, 1),
-                 kappa = round(khat, 4),
-				 true.skill = true.skill, 
-				 sensitivity = tpr,
-				 specificity = tnr,
-				 plr = plr,
-				 nlr = nlr,
-				 typeI.error = type1.error,
-				 typeII.error = type2.error,
-				 f.score = f.score,
-				 gain = gain,
+                auc = auc, 	
+	        users.accuracy = round(user2 * 100, 1),  
+	        producers.accuracy = round(prod2 * 100, 1),
+                kappa = round(khat, 4),
+		true.skill = true.skill, 
+		sensitivity = tpr,
+		specificity = tnr,
+		plr = plr,
+		nlr = nlr,
+		typeI.error = type1.error,
+		typeII.error = type2.error,
+		gini = gini,
+		f.score = f.score,
+		gain = gain,
                  matthews = mcc, 				 
                  confusion = t.xy)	
-        } else {
+    } else {
 	  acc <- list( PCC = (sum(diag(t4))/sum(t4)) * 100, 
 	             users.accuracy = round(user2 * 100, 1),  
 	             producers.accuracy = round(prod2 * 100, 1),
-                 kappa = round(khat, 4),
-                 confusion = t.xy)
+                     kappa = round(khat, 4),
+                     confusion = t.xy )
 	}			 
 	class(acc) <- c("accuracy", "list") 			   
           return( acc )
