b1be3942475be98d2df0c916ee5ce5eb13ea75db
Jeffrey Evans <jeffrey_evans@tnc.org>
2019-12-02 12:55:25 -0700

    update to colinear

---
+++ R/cor.data-data.R
@@ -0,0 +1,17 @@
+#' @name correlated data
+#' @docType data
+#'
+#' @title Various correlation structures
+#'
+#' @description a list object with various linear and nonlinear correlation structures
+#'
+#' @format A list with 4 elements containing data.frames:
+#' \describe{
+#'   \item{example 1}{two columns with nonlinear wave function relationship}
+#'   \item{example 2}{two columns with simple nonlinear relationship}
+#'   \item{example 3}{two columns with nonlinear multi-level wave function relationship}
+#'   \item{example 4}{4 coulmuns with firs two having linear relationship}
+
+#' }
+#'
+NULL
